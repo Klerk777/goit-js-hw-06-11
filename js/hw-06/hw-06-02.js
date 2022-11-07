20cfbfeb27d888cf71d496cb05966733e3b0ffa1
Volodymyr Bakumenko <Klerk.mail@gmail.com>
2022-11-07 04:30:55 +0200

    Add tasks 1 and 2 Changes to be committed: 	new file:   .prettierrc.json 	new file:   js/hw-06/hw-06-01.js 	new file:   js/hw-06/hw-06-02.js 	new file:   src/images/favicon-book.ico 	modified:   src/index.html 	new file:   src/pages/hw-06/hw-06-01.html 	new file:   src/pages/hw-06/hw-06-02.html 	new file:   src/pages/hw-06/hw-06.html 	deleted:    src/partials/example.html 	new file:   src/partials/header.html 	new file:   src/partials/home-page.html 	modified:   src/sass/_common.scss 	deleted:    src/sass/_example.scss 	new file:   src/sass/_header.scss 	new file:   src/sass/_home_page.scss 	new file:   src/sass/_task-page.scss 	modified:   src/sass/index.scss

---
+++ js/hw-06/hw-06-02.js
@@ -0,0 +1,18 @@
+//  Напиши скрипт, який для кожного елемента масиву ingredients :
+
+//     Створить окремий елемент <li> . Обов'язково використовуй метод document.createElement() .
+//     Додасть назву інгредієнта як його текстовий вміст.
+//     Додасть елементу клас item .
+//     Після чого, вставить усі <li> за одну операцію у список ul#ingredients .
+
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+const list = document.querySelector('#ingredients');
+const items = [];
+ingredients.map(ingredient => {
+  const item = document.createElement('li');
+  item.textContent = ingredient;
+  item.classList.add('item');
+  items.push(item);
+  //   list.append(item); // add item to DOM on each iteration of .map() method
+});
+list.append(...items); // add items to DOM in one action

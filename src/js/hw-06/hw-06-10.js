88556f122c9d8364fa78a44878124033c1da2f4c
Volodymyr Bakumenko <Klerk.mail@gmail.com>
2022-11-08 23:34:11 +0200

    Add task 9,10. redesign maine page Changes to be committed: 	new file:   src/js/hw-06/hw-06-09.js 	new file:   src/js/hw-06/hw-06-10.js 	modified:   src/pages/hw-06/hw-06-01.html 	modified:   src/pages/hw-06/hw-06-02.html 	modified:   src/pages/hw-06/hw-06-03.html 	modified:   src/pages/hw-06/hw-06-04.html 	modified:   src/pages/hw-06/hw-06-05.html 	modified:   src/pages/hw-06/hw-06-06.html 	modified:   src/pages/hw-06/hw-06-07.html 	modified:   src/pages/hw-06/hw-06-08.html 	new file:   src/pages/hw-06/hw-06-09.html 	new file:   src/pages/hw-06/hw-06-10.html 	modified:   src/pages/hw-06/hw-06.html 	modified:   src/partials/header.html 	modified:   src/partials/home-page.html 	modified:   src/sass/_header.scss 	modified:   src/sass/_home_page.scss 	modified:   src/sass/_hw-06.scss 	modified:   src/sass/_task-page.scss 	modified:   src/src1/js/task-10.js

---
+++ src/js/hw-06/hw-06-10.js
@@ -0,0 +1,52 @@
+// Завдання 10 (виконувати не обов'язково)
+// Напиши скрипт створення і очищення колекції елементів. Користувач вводить кількість
+// елементів в input і натискає кнопку Створити, після чого рендериться колекція. Натисненням
+// на кнопку Очистити, колекція елементів очищається.
+
+// Створи функцію createBoxes(amount), яка приймає один параметр - число. Функція створює
+// стільки < div >, скільки вказано в amount і додає їх у div#boxes.
+
+// Розміри найпершого <div> - 30px на 30px.
+// Кожен елемент після першого повинен бути ширшим і вищим від попереднього на 10px.
+// Всі елементи повинні мати випадковий колір фону у форматі HEX. Використовуй готову
+// функцію getRandomHexColor для отримання кольору.
+
+// Створи функцію destroyBoxes(), яка очищає вміст div#boxes, у такий спосіб видаляючи всі створені елементи.
+
+const refs = {
+  input: document.querySelector('#controls').firstElementChild,
+  createBtn: document.querySelector('[data-create]'),
+  destroyBtn: document.querySelector('[data-destroy]'),
+  divBox: document.querySelector('#boxes'),
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+// function randomColor() {
+//   var color = '#' + Math.random().toString(16).slice(3, 9);
+//   return color;
+// }
+
+const createBoxes = amount => {
+  amount = refs.input.value;
+  let boxSide = refs.divBox.lastElementChild === null ? 30 : parseInt(refs.divBox.lastElementChild.style.width) + 10;
+  for (let i = 0; i < amount; i++) {
+    const newDiv = document.createElement('div');
+    newDiv.style.backgroundColor = getRandomHexColor();
+    newDiv.style.width = boxSide + 'px';
+    newDiv.style.height = boxSide + 'px';
+    refs.divBox.append(newDiv);
+    boxSide += 10;
+  }
+};
+
+const destroyBoxes = () => {
+  refs.divBox.innerHTML = '';
+};
+
+refs.createBtn.addEventListener('click', () => createBoxes());
+refs.destroyBtn.addEventListener('click', () => destroyBoxes());

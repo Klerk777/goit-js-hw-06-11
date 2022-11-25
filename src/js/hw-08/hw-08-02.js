5a64d7e32e76151733393d3a0833134109921562
Volodymyr Bakumenko <Klerk.mail@gmail.com>
2022-11-25 20:13:09 +0200

    Add task 08-02, 08-03.
    Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/js/hw-08/hw-08-02.js
    	modified:   src/js/hw-08/hw-08-03.js
    	modified:   src/pages/hw-08/hw-08-02.html
    	modified:   src/pages/hw-08/hw-08-03.html
    	new file:   src/sass/_hw-08.scss
    	modified:   src/sass/index.scss
    	renamed:    src/src-o7/01-gallery.html -> src/src-07/01-gallery.html
    	renamed:    src/src-o7/02-lightbox.html -> src/src-07/02-lightbox.html
    	renamed:    src/src-o7/css/common.css -> src/src-07/css/common.css
    	renamed:    src/src-o7/css/styles.css -> src/src-07/css/styles.css
    	renamed:    src/src-o7/index.html -> src/src-07/index.html
    	renamed:    src/src-o7/js/01-gallery.js -> src/src-07/js/01-gallery.js
    	renamed:    src/src-o7/js/02-lightbox.js -> src/src-07/js/02-lightbox.js
    	renamed:    src/src-o7/js/gallery-items.js -> src/src-07/js/gallery-items.js
    	renamed:    src/src-o7/js/hw-07-01 copy.js -> src/src-07/js/hw-07-01 copy.js
    	new file:   src/src-08/01-gallery.html
    	new file:   src/src-08/02-video.html
    	new file:   src/src-08/03-feedback.html
    	new file:   src/src-08/css/01-gallery.css
    	new file:   src/src-08/css/03-feedback.css
    	new file:   src/src-08/css/common.css
    	new file:   src/src-08/index.html
    	new file:   src/src-08/js/01-gallery.js
    	new file:   src/src-08/js/02-video.js
    	new file:   src/src-08/js/03-feedback.js
    	new file:   src/src-08/js/gallery-items.js

---
+++ src/js/hw-08/hw-08-02.js
@@ -0,0 +1,29 @@
+// HTML містить < iframe > з відео для Vimeo плеєра.Напиши скрипт, який буде зберігати поточний час відтворення відео у локальне сховище і, після перезавантаження сторінки, продовжувати відтворювати відео з цього часу.
+// Розбий це завдання на декілька підзавдань:
+
+//     Ознайомся з документацією бібліотеки Vimeo плеєра.
+//     Додай бібліотеку як залежність проекту через npm .
+//     Ініціалізуй плеєр у файлі скрипта як це описано в секції pre-existing player , але враховуй, що у тебе плеєр доданий як npm пакет, а не через CDN.
+//     Вивчи документацію методу on() і почни відстежувати подію timeupdate - оновлення часу відтворення.
+//     Зберігай час відтворення у локальне сховище. Нехай ключем для сховища буде рядок "videoplayer-current-time" .
+//     Під час перезавантаження сторінки скористайся методом setCurrentTime() з метою відновлення відтворення зі збереженої позиції.
+//     Додай до проекту бібліотеку lodash.throttle і зроби так, щоб час відтворення оновлювався у сховищі не частіше, ніж раз на секунду.
+
+import Player from '@vimeo/player';
+import _ from 'lodash';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+const currentTime = localStorage.getItem(LOCALSTORAGE_KEY) ? localStorage.getItem(LOCALSTORAGE_KEY) : 0;
+
+function saveCurrentTime(e) {
+  localStorage.setItem(LOCALSTORAGE_KEY, e.seconds);
+}
+
+player.on('timeupdate', _.throttle(saveCurrentTime, 1000));
+
+player.setCurrentTime(currentTime);
+
+// player.on('pause', function (e) {
+//   console.log('paused! ', e.seconds);
+// });
---
+++ src/js/hw-08/hw-08-03.js
@@ -0,0 +1,46 @@
+// HTML містить розмітку форми. Напиши скрипт, який буде зберігати значення полів у локальне сховище, коли користувач щось друкує.
+
+// Розбий це завдання на декілька підзавдань:
+
+// 1. Відстежуй на формі подію input , і щоразу записуй у локальне сховище об'єкт з полями email і message,
+//      у яких зберігай поточні значення полів форми. Нехай ключем для сховища буде рядок "feedback-form-state".
+// 2. Під час завантаження сторінки перевіряй стан сховища, і якщо там є збережені дані, заповнюй ними поля
+//      форми.В іншому випадку поля повинні бути порожніми.
+// 3. Під час сабміту форми очищуй сховище і поля форми, а також виводь у консоль об'єкт з полями email,
+//      message та їхніми поточними значеннями.
+// 4. Зроби так, щоб сховище оновлювалось не частіше, ніж раз на 500 мілісекунд. Для цього додай до
+//      проекту і використовуй бібліотеку lodash.throttle.
+
+import _ from 'lodash';
+const form = document.querySelector('.feedback-form');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+function onInputHandler() {
+  const currentInputState = {};
+  currentInputState.email = form.email.value;
+  currentInputState.message = form.message.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(currentInputState));
+}
+
+function onSubmitHandler(e) {
+  e.preventDefault();
+  const submitData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+  console.log('Submit form!');
+  console.table(submitData);
+  e.currentTarget.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+
+function onPageLoad() {
+  const parsedValue = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+  if (parsedValue) {
+    form.email.value = parsedValue.email;
+    form.message.value = parsedValue.message;
+  }
+}
+
+form.addEventListener('input', _.throttle(onInputHandler, 500));
+form.addEventListener('submit', onSubmitHandler);
+
+//run
+onPageLoad();

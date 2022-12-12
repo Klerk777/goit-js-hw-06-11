bdb15587f326c4bd25f824f917a1e8d1f6297f55
Volodymyr Bakumenko <Klerk.mail@gmail.com>
2022-12-13 01:19:39 +0200

    Adds task 09-03 Changes to be committed: 	modified:   src/js/hw-09/hw-09-03.js

--- src/js/hw-09/hw-09-03.js
+++ src/js/hw-09/hw-09-03.js
@@ -1,8 +1,41 @@
+import { Notify } from 'notiflix';
+
+const refs = {
+  form: document.querySelector('.form'),
+  delay: document.querySelector('.delay'),
+  step: document.querySelector('.step'),
+  amount: document.querySelector('.amount'),
+};
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function onSucces({ position, delay }) {
+  Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+}
+
+function onError({ position, delay }) {
+  Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+}
+
+function onSubmitHandler(e) {
+  e.preventDefault();
+
+  let delay = Number(e.target.delay.value);
+  for (let i = 1; i <= e.target.amount.value; i++) {
+    createPromise(i, delay).then(onSucces).catch(onError);
+    delay += Number(e.target.step.value);
   }
 }
+
+refs.form.addEventListener('submit', onSubmitHandler);

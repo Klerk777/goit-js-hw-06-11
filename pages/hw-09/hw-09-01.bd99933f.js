fb65e6116b66864a6c3d713592b0d3fc1907da4d
Klerk777 <Klerk.mail@gmail.com>
2022-12-13 01:34:23 +0000

    Deploying to gh-pages from @ Klerk777/goit-js-hw-06-11@863cc33a84b87f25c9fce116a5c0707753adc8e1 🚀

---
+++ pages/hw-09/hw-09-01.bd99933f.js
@@ -0,0 +1,2 @@
+let t;const e=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]");function a(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}e.addEventListener("click",(function(){a(),t=setInterval(a,1e3),e.disabled=!0})),n.addEventListener("click",(function(){t&&(clearInterval(t),e.disabled=!1)}));
+//# sourceMappingURL=hw-09-01.bd99933f.js.map
